8598e1bd924e638833cd92c9394b3467ffc67728
Cillian de Róiste <deroiste@syslab.com>
2016-05-10 17:06:31 +0200

    Add configuration

---
+++ ExtensionSettings.php
@@ -0,0 +1,231 @@
+<?php
+# http://www.mediawiki.org/wiki/Manual:Wiki_family#Shared_Settings
+#####
+##### Extension Directory Variables
+#####
+
+# This is required for infoboxes to work
+/* require_once( "$IP/extensions/ParserFunctions/ParserFunctions.php" ); */
+wfLoadExtension( 'ParserFunctions' );
+$wgPFEnableStringFunctions = true;
+$wgPFStringLengthLimit = 5000;
+
+# This is required so that most filetypes can be uploaded. Some are still blacklisted though.
+$wgStrictFileExtensions = false;
+
+# Adding this in the SMW config to allow uploading of PDF files, this
+# already works on the old version so I don't see why it is now
+# required
+$wgAllowExternalImages = true;
+$wgFileExtensions[] = 'pdf';
+
+# For js-kit-ratings
+/* require_once( "$IP/extensions/StubManager/StubManager.php" ); */
+/* require_once( "$IP/extensions/JSKitRating/JSKitRating.php" ); */
+/* wfLoadExtension( 'JSKitRating' ); */
+
+
+# For MediawikiPlayer
+#require_once("$IP/extensions/MediawikiPlayer/MediawikiPlayer.php");
+
+# To list a category of pages
+# include("$IP/extensions/intersection/DynamicPageList.php");
+#include("$IP/extensions/DynamicPageList/DynamicPageList2.php");
+$wgRawHtml=true;
+
+# http://www.mediawiki.org/wiki/Extension:AuthorInfo
+#require_once("$IP/extensions/AuthorInfo/AuthorInfo.php");
+
+# Enable CategoryTree: http://www.mediawiki.org/wiki/Extension:CategoryTree
+$wgUseAjax = true;
+/* require_once("{$IP}/extensions/CategoryTree/CategoryTree.php"); */
+/* wfLoadExtension( 'CategoryTree' ); */
+
+
+# http://www.mediawiki.org/wiki/Extension:Cite/Cite.php
+wfLoadExtension( 'Cite' );
+/* require_once("$IP/extensions/Cite/SpecialCite.php"); */
+
+
+# LdapAuthentication
+# http://www.mediawiki.org/wiki/Extension:LDAP_Authentication/Configuration
+#  require_once( "$IP/extensions/LdapAuthentication/LdapAuthentication.php" );
+#  $wgAuth = new LdapAuthenticationPlugin();
+#  $wgLDAPDomainNames = array( "osha" );
+#  $wgLDAPServerNames = array( "osha" => "localhost" );
+#  $wgLDAPEncryptionType = array("osha" => "clear" );
+#  $wgLDAPBaseDNs = array( "osha" => "ou=staff,o=3G,dc=osha,dc=europa,dc=eu" );
+#  $wgLDAPSearchAttributes = array( "osha" =>"mail" );
+#  $wgLDAPLowerCaseUsername = array( "osha" => true );
+#  $wgLDAPProxyAgent = array( "osha"=>"cn=reader,dc=osha,dc=europa,dc=eu");
+#  # Restrict to the OSHWIKIEditors group
+#  $wgLDAPGroupUseFullDN = array( "osha"=>true );
+#  $wgLDAPGroupObjectclass = array( "osha"=>"groupOfUniqueNames" );
+#  $wgLDAPGroupAttribute = array( "osha"=>"uniquemember" );
+#  $wgLDAPGroupSearchNestedGroups = array( "osha"=>false );
+#  $wgLDAPGroupNameAttribute = array( "osha"=>"cn" );
+#  $wgLDAPRequiredGroups = array( "osha"=>array("cn=oshwikieditors,ou=europe,ou=staff,o=3G,dc=osha,dc=europa,dc=eu"));
+#  $wgLDAPPreferences = array( "osha"=>array( "email"=>"mail","realname"=>"cn","nickname"=>"cn") );
+#  require_once("/srv/httpd/oshawiki.batou/work/mediawiki/secrets.php");
+#  $wgLDAPPort = array('osha' => 1389,);
+#  $wgLDAPDebug = 0;
+#  $wgDebugLogGroups['ldap'] = '/tmp/debug.log';
+#  
+#  $wgGroupPermissions['*']['createaccount'] = false;
+#  $wgGroupPermissions['*']['edit'] = false;
+
+  
+# http://www.mediawiki.org/wiki/Extension:ContributionCredits
+require_once("$IP/extensions/ContributionCredits/ContributionCredits.php");
+
+# optional: set this variable to the Section Header for the list of Contributors. Shown the default value.
+$wgContributionCreditsHeader = "= Contributors =\n ";
+
+require_once("$IP/extensions/UserContributions/UserContributions.php");
+
+/* require_once("$IP/extensions/SocialProfile/SocialProfile.php"); */
+/* $wgExtraNamespaces[NS_USER_PROFILE] = 'User_profile'; */
+/* $wgExtraNamespaces[NS_USER_WIKI] = 'UserWiki'; */
+
+# Enable subcategories
+$wgNamespacesWithSubpages[NS_CATEGORY] = true;
+
+# http://www.mediawiki.org/wiki/Extension:AddThis
+require_once("$IP/extensions/AddThis/AddThis.php");
+$wgAddThispubid = 'osha';
+$wgAddThisHeader = true;
+$wgAddThisSidebar = false;
+
+# http://www.mediawiki.org/wiki/Extension:Variables
+require_once("$IP/extensions/Variables/Variables.php");
+
+include_once("$IP/extensions/Validator/Validator.php");
+
+include_once("$IP/extensions/SemanticMediaWiki/SemanticMediaWiki.php");
+enableSemantics('oshwiki.eu');
+## in localsetting: now: $smwgDefaultStore = 'SMWSQLStore2';
+
+# Does not work with the RefToolbar/Cite editor extension and is
+# unmaintained and unused anyway (there are no uses of #tree or
+# #generateTree in the content)
+# http://wiki.ontoprise.com/smwforum/index.php/Help:TreeView_extension
+#include_once("$IP/extensions/Treeview/Treeview.php");
+
+#http://www.mediawiki.org/wiki/Extension:Semantic_Forms
+include_once("$IP/extensions/SemanticForms/SemanticForms.php");
+# Note the URL must be accessible to the local Apache, without auth
+$sfgAutocompletionURLs['osha-property-list'] = "$wgServer$wgScriptPath/property-lists/osha.json";
+$sfgAutocompletionURLs['isco-property-list'] = "$wgServer$wgScriptPath/property-lists/isco.json";
+$sfgAutocompletionURLs['nace-property-list'] = "$wgServer$wgScriptPath/property-lists/nace.json";
+
+# http://www.mediawiki.org/wiki/Extension:Semantic_Internal_Objects
+include_once("$IP/extensions/SemanticInternalObjects/SemanticInternalObjects.php");
+
+# http://www.mediawiki.org/wiki/Extension:Replace_Text
+require_once( "$IP/extensions/ReplaceText/ReplaceText.php" );
+
+# $smwgQMaxInlineLimit = 5;
+# $smwgQPrintoutLimit = 5;
+
+###
+# Sets whether or not the 'printouts' textarea should have autocompletion
+# on property names.
+##
+$smwgAutocompleteInSpecialAsk = false;
+
+$smwgPageSpecialProperties = array( '_MDAT', '_CDAT' );
+
+# ##
+# Whether to autocomplete on all characters in a string, not just the
+# beginning of words - this is especially important for Unicode strings,
+# since the use of the '\b' regexp character to match on the beginnings
+# of words fails for them.
+# ##
+$sfgAutocompleteOnAllChars = true;
+
+##
+# The NACE properties currently have the largest amount of values (just under 3300) 
+##
+$sfgMaxAutocompleteValues = 3300;
+$codeMatchesURL = "/mediawiki/property-lists/code-matches.php";
+
+$codeMatchesDataTypes = array( 'osha', 'isco', 'nace' );
+$codeMatchesLanguages = array( 'en', 'bg', 'cs', 'da', 'el', 'es', 'et', 'fi', 'fr', 'hu', 'it', 'lt', 'lv', 'mt', 'nl', 'pl', 'pt', 'ro', 'sh', 'sk', 'sl', 'sv', 'tr');
+ 
+foreach ( $codeMatchesDataTypes as $dataType ) {
+	foreach ( $codeMatchesLanguages as $language ) {
+		$sfgAutocompletionURLs["code-matches-$dataType-$language"] =
+			"$codeMatchesURL?data-type=$dataType&language=$language&substring=<substr>";
+	}
+}
+
+# FIXME
+# Jos WikiWorks
+# Display properties just below the category box
+/* $wgHooks['SkinAfterContent'][] = 'PropSubCat'; */
+
+
+function PropSubCat (&$data, $skin) {
+		global $wgOut;
+		$wTemplate = '{{DisplayProperties}}';
+		$wikiOutput=$wgOut->parse($wTemplate);
+		$data = $wikiOutput;
+return true;
+}
+
+##
+# Ratings extension
+# https://www.mediawiki.org/wiki/Extension:VoteNY
+##
+require_once("$IP/extensions/VoteNY/Vote.php"); 
+$wgGroupPermissions['*']['vote'] = true; // Anonymous users can vote
+
+wfLoadExtension( 'UserMerge' ); 
+// By default nobody can use this function, enable for bureaucrat?
+$wgGroupPermissions['bureaucrat']['usermerge'] = true;
+ 
+// optional: default is array( 'sysop' )
+// $wgUserMergeProtectedGroups = array( 'groupname' );
+
+$wgShowExceptionDetails = true;
+
+require_once("$IP/extensions/SidebarLanguagelink/SidebarLanguagelink.php");
+
+require_once "$IP/extensions/wiki-seo-1.2.1/WikiSEO.php";
+
+
+/* $wgDefaultUserOptions['wikieditor-preview'] = 1; */
+
+/* ini_set('display_errors', 'on'); */
+/* error_reporting(E_ALL); */
+
+$wgShowExceptionDetails = false; 
+
+# http://semantic-mediawiki.org/wiki/Help:Installation#Upgrading_SMW_1.6.x_and_SMW_1.7.x
+# The migration to SMWSQLStore3 has been performed, but an error is displayed when viewing any page when it is enabled
+$smwgDefaultStore = 'SMWSQLStore2';
+
+$wgSpecialVersionShowHooks = true;
+
+
+
+/* $wgUserrightsInterwikiDelimiter = '#'; */
+
+
+require_once "$IP/extensions/VisualEditor/VisualEditor.php";
+
+// Enable by default for everybody
+$wgDefaultUserOptions['visualeditor-enable'] = 1;
+
+// Don't allow users to disable it
+$wgHiddenPrefs[] = 'visualeditor-enable';
+
+$wgVirtualRestConfig['modules']['parsoid'] = array(
+  // URL to the Parsoid instance
+  // Use port 8142 if you use the Debian package
+  'url' => 'http://localhost:8000',
+  // Parsoid "domain", see below (optional)
+  'domain' => 'oshwiki.goibh.eu',
+  // Parsoid "prefix", see below (optional)
+  'prefix' => 'oshwiki.goibh.eu'
+);
---
+++ GeneralSettings.php
@@ -0,0 +1,96 @@
+<?php
+
+$wgSitename			= "OSHWiki";
+
+## The URL base path to the directory containing the wiki;
+## defaults for all runtime URL paths are based off of this.
+## For more information on customizing the URLs please see:
+## http://www.mediawiki.org/wiki/Manual:Short_URL
+$wgScriptPath		= "/mediawiki";
+$wgScriptExtension	= ".php";
+
+## The relative URL path to the skins directory
+/* $wgStylePath		= "$wgScriptPath/skins"; */
+
+## The relative URL path to the logo.  Make sure you change this from the default,
+## or else you'll overwrite your logo when you upgrade!
+$wgLogo				= "";
+
+## UPO means: this is also a user preference option
+
+$wgEnableEmail		= true;
+$wgEnableUserEmail	= true; # UPO
+
+$wgEnotifUserTalk = true; # UPO
+$wgEnotifWatchlist = true; # UPO
+$wgEmailAuthentication = true;
+
+
+# MySQL specific settings
+$wgDBprefix			= "";
+
+# MySQL table options to use during installation or update
+$wgDBTableOptions	= "ENGINE=InnoDB, DEFAULT CHARSET=binary";
+
+# Experimental charset support for MySQL 4.1/5.0.
+$wgDBmysql5 = true;
+
+## Shared memory settings
+$wgMainCacheType = CACHE_NONE;
+$wgMemCachedServers = array();
+
+## To enable image uploads, make sure the 'images' directory
+## is writable, then set this to true:
+$wgEnableUploads	   = true;
+$wgUseImageMagick = true;
+
+## If you use ImageMagick (or any other shell command) on a
+## Linux server, this will need to be set to the name of an
+## available UTF-8 locale
+$wgShellLocale = "en_US.utf8";
+
+## If you want to use image uploads under safe mode,
+## create the directories images/archive, images/thumb and
+## images/temp, and make them all writable. Then uncomment
+## this, if it's not already uncommented:
+# $wgHashedUploadDirectory = false;
+
+## If you have the appropriate support software installed
+## you can enable inline LaTeX equations:
+$wgUseTeX			= false;
+
+## Set $wgCacheDirectory to a writable directory on the web server
+## to make your wiki go slightly faster. The directory should not
+## be publically accessible from the web.
+#$wgCacheDirectory = "$IP/cache";
+
+/* $wgLocalInterwiki	= strtolower( $wgSitename ); */
+
+$wgLanguageCode = "en";
+
+## Default skin: you can change the default skin. Use the internal symbolic
+## names, ie 'vector', 'monobook':
+
+wfLoadSkin( 'MonoBook' );
+$wgDefaultSkin = 'MonoBook';
+
+/* wfLoadSkin( 'Osha' ); */
+/* $wgDefaultSkin = 'Osha'; */
+
+
+## For attaching licensing metadata to pages, and displaying an
+## appropriate copyright notice / icon. GNU Free Documentation
+## License and Creative Commons licenses are supported so far.
+# $wgEnableCreativeCommonsRdf = true;
+$wgRightsPage = ""; # Set to the title of a wiki page that describes your license/copyright
+$wgRightsUrl = "";
+$wgRightsText = "";
+$wgRightsIcon = "";
+# $wgRightsCode = ""; # Not yet used
+
+
+# When you make changes to this configuration file, this will make
+# sure that cached pages are cleared.
+$wgCacheEpoch = max( $wgCacheEpoch, gmdate( 'YmdHis', @filemtime( __FILE__ ) ) );
+$wgUseSquid = true;
+$wgUsePrivateIPs = true;
