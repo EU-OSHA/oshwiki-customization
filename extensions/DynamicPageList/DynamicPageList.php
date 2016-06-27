889318ace2f960e07bf5ecc54ce55d7bc83f041b
Cillian de Róiste <deroiste@syslab.com>
2016-06-27 19:11:22 +0200

    Upgrade DPL to version 3

--- extensions/DynamicPageList/DynamicPageList.php
+++ extensions/DynamicPageList/DynamicPageList.php
@@ -1,85 +1,126 @@
 <?php
+/**
+ * DynamicPageList3
+ * DynamicPageList3 Master File
+ *
+ * @author		IlyaHaykinson, Unendlich, Dangerville, Algorithmix, Theaitetos, Alexia E. Smith
+ * @license		GPL
+ * @package		DynamicPageList3
+ *
+**/
 
-/*
-   This is the main entry point for DPL installations
-   --------------------------------------------------
-
-	In your LocalSettings.php you write:
-
-        require_once( "$IP/extensions/DynamicPageList/DynamicPageList.php" );
-
-	DPL has many features. If you do not want to enable all of them
-	you can define the "Level of Functional Richness" you want to offer to your users.
-	Make the following call in your LocalSettings.php after the require_once() statement:
-
-        ExtDynamicPageList::setFunctionalRichness(  <n>  );
-
-	where <n> is a number between 0 and 4.
-
-	If your wiki has been using Extension:Intersection you must UNINSTALL that
-	extension before you can use DynamicPageList (DPL).
-
-	DPL is downward compatible with Extension:Intersection. It registers
-	the tag <DynamicPageList> and behaves exactly like ExtensionIntersection
-	(without any additional functionality).
-
-	Note that there are subtle differences in the default settings for order and ordermethod
-	between <DynamicPageList> and <DPL>. Therefore you cannot always expect the same result
-	if you change these tags.
-
-	If your wiki has been using Extension:Intersection you may want to install
-	DPL in parallel to that extension before you replace Extension:Intersection by DPL.
-	In this case you must require_once("DynamicPageListMigration.php") instead of the current file
-	in your LocalSettings.php.
-
-	Some functions of DPL are quite useful but if abused (by error or bad intention) they may put severe load
-	on your server / database. For wikis up to 10.000 pages this is normally not a problem,
-	but with larger wikis some care is advisable.
-
-	By default the RichnessLevel is set to 4 (= activate whole set of functions).
-
-	Use a different value if you do not feel comfortable with this:
-	-  level=0 will not allow any additional functionality (compared to Extension:Intersection).
-	-  level=1 brings a series of improvements which will not affect performance
-	-  level=2 brings some additional features which are roughly on the same level of database load
-			   as the basic functionality; also contains content transclusion (which may require
-               the DPL-cache on huge websites)
-	-  level=3 brings more new features: selection based on regular expressions, queries on
-	           revision level
-	-  level=4 adds a few additional features which are useful for private websites (like batch updates)
-               but should not be made available on huge public websites.
-
-*/
-
-// we register the tag <dpl> and function #dpl
-// we also register the tag <DynamicPageList> because DPL is downward compatible with Extension:Intersection
-// This means that your LocalSettings.php MUST NO LONGER include Extension:Intersection;
-
-if ( !defined( 'MEDIAWIKI' ) ) {
-    die( 'This is not a valid entry point to MediaWiki.' );
+if (!defined('MEDIAWIKI')) {
+    die('This is not a valid entry point to MediaWiki.');
 }
 
-$wgExtensionFunctions[]        = array( 'ExtDynamicPageList', 'setupDPL' );
-$wgHooks['LanguageGetMagic'][] = 'ExtDynamicPageList__languageGetMagic';
+/******************************************/
+/* Credits								  */
+/******************************************/
+define('DPL_VERSION', '3.0.9');
 
-$wgMessagesDirs['DynamicPageList'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['DynamicPageList'] = __DIR__ . '/DynamicPageList.i18n.php';
-
-$DPLVersion = '2.3.0';
-
-$wgExtensionCredits['parserhook'][] = array(
+$credits = [
 	'path' 				=> __FILE__,
-	'name' 				=> 'DynamicPageList (third party)',
-	'author' 			=>  '[http://de.wikipedia.org/wiki/Benutzer:Algorithmix Gero Scholz]',
-	'url' 				=> 'https://www.mediawiki.org/wiki/Extension:DynamicPageList_(third-party)',
+	'name' 				=> 'DynamicPageList3',
+	'author' 			=> ['[http://de.wikipedia.org/wiki/Benutzer:Algorithmix Gero Scholz]', 'Alexia E. Smith'],
+	'url' 				=> 'https://www.mediawiki.org/wiki/Extension:DynamicPageList3',
 	'descriptionmsg' 	=> 'dpl-desc',
-  	'version' 			=> $DPLVersion
-  );
+	'license-name'		=> 'GPL-3.0',
+	'version' 			=> DPL_VERSION
+];
+$wgExtensionCredits['parserhook'][] = $credits;
 
-require_once( 'DPLSetup.php' );
+/******************************************/
+/* Language Strings, Page Aliases, Hooks  */
+/******************************************/
+$extDir = __DIR__;
 
-ExtDynamicPageList::$DPLVersion = $DPLVersion;
+$wgAvailableRights[] = 'dpl_param_update_rules';
+$wgAvailableRights[] = 'dpl_param_delete_rules';
 
-// use full functionality by default
-ExtDynamicPageList::setFunctionalRichness( 4 );
+$wgMessagesDirs['DynamicPageList']					= "{$extDir}/i18n";
+$wgExtensionMessagesFiles['DynamicPageList']		= "{$extDir}/DynamicPageList.i18n.php";
+$wgExtensionMessagesFiles['DynamicPageListMagic']	= "{$extDir}/DynamicPageList.i18n.magic.php";
 
+$wgAutoloadClasses['DynamicPageListHooks']			= "{$extDir}/DynamicPageList.hooks.php";
+$wgAutoloadClasses['DPL\Article']					= "{$extDir}/classes/Article.php";
+$wgAutoloadClasses['DPL\Config']					= "{$extDir}/classes/Config.php";
+$wgAutoloadClasses['DPL\DynamicPageList']			= "{$extDir}/classes/DynamicPageList.php";
+$wgAutoloadClasses['DPL\Logger']					= "{$extDir}/classes/Logger.php";
+$wgAutoloadClasses['DPL\ListMode']					= "{$extDir}/classes/ListMode.php";
+$wgAutoloadClasses['DPL\Logger']					= "{$extDir}/classes/Logger.php";
+$wgAutoloadClasses['DPL\LST']						= "{$extDir}/classes/LST.php";
+$wgAutoloadClasses['DPL\Parameters']				= "{$extDir}/classes/Parameters.php";
+$wgAutoloadClasses['DPL\ParametersData']			= "{$extDir}/classes/ParametersData.php";
+$wgAutoloadClasses['DPL\Parse']						= "{$extDir}/classes/Parse.php";
+$wgAutoloadClasses['DPL\Query']						= "{$extDir}/classes/Query.php";
+$wgAutoloadClasses['DPL\Variables']					= "{$extDir}/classes/Variables.php";
+
+if (isset($dplMigrationTesting) && $dplMigrationTesting === true) {
+	$wgHooks['ParserFirstCallInit'][]					= 'DynamicPageListHooks::setupMigration';
+} else {
+	$wgHooks['ParserFirstCallInit'][]					= 'DynamicPageListHooks::onParserFirstCallInit';
+}
+$wgHooks['LoadExtensionSchemaUpdates'][]				= 'DynamicPageListHooks::onLoadExtensionSchemaUpdates';
+
+//Give sysops permission to use updaterules and deleterules by default.
+if (!isset($wgGroupPermissions['sysop']['dpl_param_update_rules'])) {
+	$wgGroupPermissions['sysop']['dpl_param_update_rules'] = true;
+}
+if (!isset($wgGroupPermissions['sysop']['dpl_param_delete_rules'])) {
+	$wgGroupPermissions['sysop']['dpl_param_delete_rules'] = true;
+}
+
+/******************************************/
+/* Final Setup                            */
+/******************************************/
+
+//By default all setup namespaces are used when DPL initializes.  Customize this setting with an array of namespace constants to restrict DPL to work only in those namespaces.
+/* Example, restrict DPL to look only in the Main and Project namespaces.
+	$wgDplSettings['allowedNamespaces'] = [
+		NS_MAIN,
+		NS_PROJECT
+	];
+*/
+$wgDplSettings['allowedNamespaces'] = null;
+
+//Set this to true to ignore 'maxCategoryCount' and allow unlimited categories.  Please note that large amounts of categories in a query can slow down or crash servers.
+$wgDplSettings['allowUnlimitedCategories'] = false;
+
+//Set this to true to ignore 'maxResultCount' and allow unlimited results.  Please note that large result sets may result in slow or failed page loads.
+$wgDplSettings['allowUnlimitedResults'] = false;
+
+//Set DPL to always behave like Extension:Intersection.
+$wgDplSettings['behavingLikeIntersection'] = false;
+
+//Maximum length to format a list of articles chunked by letter as bullet list, if list bigger or columnar format user.(Same as cut off argument for CategoryViewer::formatList()).
+$wgDplSettings['categoryStyleListCutoff'] = 6;
+
+//This does something with preventing DPL from "looking" at these categories.
+$wgDplSettings['fixedCategories'] = [];
+
+//Set the level of parameters available to end users.
+if (isset($dplMigrationTesting) && $dplMigrationTesting === true) {
+	$wgDplSettings['functionalRichness'] = 0;
+} else {
+	$wgDplSettings['functionalRichness'] = 3;
+}
+
+
+//Maximum number of categories to allow in queries.
+$wgDplSettings['maxCategoryCount'] = 4;
+
+//Minimum number of categories to allow in queries.
+$wgDplSettings['minCategoryCount'] = 0;
+
+//Maximum number of results to return from a query.
+$wgDplSettings['maxResultCount'] = 500;
+
+//Do recursive tag parsing on <dpl> parser tags converting tags and functions such as magic words like {{PAGENAME}}.  This is similar to the {{#dpl}} parser function call, but may not work exactly the same in all cases.
+$wgDplSettings['recursiveTagParse'] = false;
+
+//Set this to true to allow DPL to run from protected pages only.  This is recommend if wiki administrators are having issues with malicious users creating computationally intensive queries.
+$wgDplSettings['runFromProtectedPagesOnly'] = false;
+
+//Handle <section> tags in an entirely broken manner.  This is disabled by default as version 3.0.8 because it is unknown what that feature was actually supposed to do.
+$wgDplSettings['handleSectionTag'] = false;
+?>
