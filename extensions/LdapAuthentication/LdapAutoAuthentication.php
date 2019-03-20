04442b872c8e294117d6d4dd6a6c1e650890b78f
Cillian de Roiste <deroiste@syslab.com>
2019-03-20 14:27:15 +0000

    LdapAuthentication: Update

--- extensions/LdapAuthentication/LdapAutoAuthentication.php
+++ extensions/LdapAuthentication/LdapAutoAuthentication.php
@@ -5,48 +5,48 @@ class LdapAutoAuthentication {
 	/**
 	 * Does the web server authentication piece of the LDAP plugin.
 	 *
-	 * @param $user User
-	 * @param $result bool
+	 * @param User $user
+	 * @param bool|null &$result
 	 * @return bool
 	 */
 	public static function Authenticate( $user, &$result = null ) {
-		/**
-		 * @var $wgAuth LdapAuthenticationPlugin
-		 */
-		global $wgAuth;
+		$ldap = LdapAuthenticationPlugin::getInstance();
 
-		$wgAuth->printDebug( "Entering AutoAuthentication.", NONSENSITIVE );
+		$ldap->printDebug( "Entering AutoAuthentication.", NONSENSITIVE );
 
 		if ( $user->isLoggedIn() ) {
-			$wgAuth->printDebug( "User is already logged in.", NONSENSITIVE );
+			$ldap->printDebug( "User is already logged in.", NONSENSITIVE );
 			return true;
 		}
 
-		$wgAuth->printDebug( "User isn't logged in, calling setup.", NONSENSITIVE );
+		$ldap->printDebug( "User isn't logged in, calling setup.", NONSENSITIVE );
 
 		// Let regular authentication plugins configure themselves for auto
 		// authentication chaining
-		$wgAuth->autoAuthSetup();
+		$ldap->autoAuthSetup();
 
-		$autoauthname = $wgAuth->getConf( 'AutoAuthUsername' );
-		$wgAuth->printDebug( "Calling authenticate with username ($autoauthname).", NONSENSITIVE );
+		$autoauthname = $ldap->getConf( 'AutoAuthUsername' );
+		$ldap->printDebug( "Calling authenticate with username ($autoauthname).", NONSENSITIVE );
 
 		// The user hasn't already been authenticated, let's check them
-		$authenticated = $wgAuth->authenticate( $autoauthname, '' );
+		$authenticated = $ldap->authenticate( $autoauthname, '' );
 		if ( !$authenticated ) {
 			// If the user doesn't exist in LDAP, there isn't much reason to
 			// go any further.
-			$wgAuth->printDebug( "User wasn't found in LDAP, exiting.", NONSENSITIVE );
+			$ldap->printDebug( "User wasn't found in LDAP, exiting.", NONSENSITIVE );
 			return false;
 		}
 
 		// We need the username that MediaWiki will always use, not necessarily the one we
 		// get from LDAP.
-		$mungedUsername = $wgAuth->getCanonicalName( $autoauthname );
+		$mungedUsername = $ldap->getCanonicalName( $autoauthname );
 
-		$wgAuth->printDebug( "User exists in LDAP; finding the user by name ($mungedUsername) in MediaWiki.", NONSENSITIVE );
+		$ldap->printDebug(
+			"User exists in LDAP; finding the user by name ($mungedUsername) in MediaWiki.",
+			NONSENSITIVE
+		);
 		$localId = User::idFromName( $mungedUsername );
-		$wgAuth->printDebug( "Got id ($localId).", NONSENSITIVE );
+		$ldap->printDebug( "Got id ($localId).", NONSENSITIVE );
 
 		// Is the user already in the database?
 		if ( !$localId ) {
@@ -56,11 +56,11 @@ class LdapAutoAuthentication {
 				return false;
 			}
 		} else {
-			$wgAuth->printDebug( "User exists in local database, logging in.", NONSENSITIVE );
+			$ldap->printDebug( "User exists in local database, logging in.", NONSENSITIVE );
 			$user->setID( $localId );
 			$user->loadFromId();
 			$user->setCookies();
-			$wgAuth->updateUser( $user );
+			$ldap->updateUser( $user );
 			wfSetupSession();
 			$result = true;
 		}
@@ -69,54 +69,46 @@ class LdapAutoAuthentication {
 	}
 
 	/**
-	 * @param $user User
-	 * @param $mungedUsername String
+	 * @param User $user
+	 * @param string $mungedUsername
 	 * @return bool
 	 */
 	public static function attemptAddUser( $user, $mungedUsername ) {
-		/**
-		 * @var $wgAuth LdapAuthenticationPlugin
-		 */
-		global $wgAuth;
+		$ldap = LdapAuthenticationPlugin::getInstance();
 
-		if ( !$wgAuth->autoCreate() ) {
-			$wgAuth->printDebug( "Cannot automatically create accounts.", NONSENSITIVE );
+		if ( !$ldap->autoCreate() ) {
+			$ldap->printDebug( "Cannot automatically create accounts.", NONSENSITIVE );
 			return false;
 		}
 
-		$wgAuth->printDebug( "User does not exist in local database; creating.", NONSENSITIVE );
+		$ldap->printDebug( "User does not exist in local database; creating.", NONSENSITIVE );
 		// Checks passed, create the user
 		$user->loadDefaults( $mungedUsername );
 		$status = $user->addToDatabase();
 		if ( $status !== null && !$status->isOK() ) {
-			$wgAuth->printDebug( "Creation failed: " . $status->getWikiText(), NONSENSITIVE );
+			$ldap->printDebug( "Creation failed: " . $status->getWikiText(), NONSENSITIVE );
 			return false;
 		}
-		$wgAuth->initUser( $user, true );
+		$ldap->initUser( $user, true );
 		$user->setCookies();
 		wfSetupSession();
 		# Update user count
 		$ssUpdate = new SiteStatsUpdate( 0, 0, 0, 0, 1 );
 		$ssUpdate->doUpdate();
-		# Notify hooks (e.g. Newuserlog)
-		wfRunHooks( 'AuthPluginAutoCreate', array( $user ) );
 
 		return true;
 	}
 
 	/**
 	 * No logout link in MW
-	 * @param $personal_urls array
-	 * @param $title Title
+	 * @param array &$personal_urls
+	 * @param Title $title
 	 * @return bool
 	 */
 	public static function NoLogout( &$personal_urls, $title ) {
-		/**
-		 * @var $wgAuth LdapAuthenticationPlugin
-		 */
-		global $wgAuth;
+		$ldap = LdapAuthenticationPlugin::getInstance();
 
-		$wgAuth->printDebug( "Entering NoLogout.", NONSENSITIVE );
+		$ldap->printDebug( "Entering NoLogout.", NONSENSITIVE );
 		unset( $personal_urls['logout'] );
 		return true;
 	}
