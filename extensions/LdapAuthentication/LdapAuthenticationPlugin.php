b9dcee395543c83bc9e500325b05a993181f397d
Cillian de Roiste <deroiste@syslab.com>
2020-01-22 17:02:57 +0000

    LdapAuth: Hardcode the domain to fix pw reset
    Refs #FLOW-955

--- extensions/LdapAuthentication/LdapAuthenticationPlugin.php
+++ extensions/LdapAuthentication/LdapAuthenticationPlugin.php
@@ -1259,7 +1259,8 @@ class LdapAuthenticationPlugin extends AuthPlugin {
 		# We must set a user option if we want token based logins to work
 		if ( $user->getToken( false ) ) {
 			$this->printDebug( "User has a token, setting domain in user options.", NONSENSITIVE );
-			self::saveDomain( $user, $_SESSION['wsDomain'] );
+			// self::saveDomain( $user, $_SESSION['wsDomain'] );
+			self::saveDomain( $user, "osha" );
 		}
 
 		# Let other extensions update the user
